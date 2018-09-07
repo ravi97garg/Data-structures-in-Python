dabfe2f0e70a7f079ad715c6b44e96d38e93b1ab
Ravi <ravi97garg@gmail.com>
2018-09-07 20:09:50 +0530

    FenwickTree completed

--- T5_BinaryIndexedTree.py
+++ T5_BinaryIndexedTree.py
@@ -2,9 +2,12 @@
 # Date Created: 7 September 2018
 # Created by Ravi garg
 
+# Note: The indexing done here is 1-based indexing
+
 
 class FenwickTree:
     def __init__(self, arr):
+        self.__inputArr = arr
         self.binaryIndexedTree = [0] * (len(arr) + 1)
         for i in range(1, len(arr) + 1):
             self.updateBinaryIndexedTree(arr[i - 1], i)
@@ -14,8 +17,14 @@ class FenwickTree:
             self.binaryIndexedTree[index] += val
             index = self.getNext(index)
 
+    def updateOriginalArray(self, val, index):
+        val = val - self.__inputArr[index - 1]
+        self.__inputArr[index - 1] = val + self.__inputArr[index - 1]
+        while index < len(self.binaryIndexedTree):
+            self.binaryIndexedTree[index] += val
+            index = self.getNext(index)
+
     def getSum(self, index):
-        index = index + 1
         sums = 0
         while index > 0:
             sums += self.binaryIndexedTree[index]
@@ -30,11 +39,11 @@ class FenwickTree:
 
 
 if __name__ == "__main__":
-    treeObj = FenwickTree([1, 2, 3, 4, -5, 6, 7])
-    print(treeObj.getSum(0))
-    print(treeObj.getSum(1))
-    print(treeObj.getSum(2))
-    print(treeObj.getSum(3))
-    print(treeObj.getSum(4))
+    treeObj = FenwickTree([1, 2, 3, 4, 0, 6, 7])
+
+    # Get sum of first 5 elements
     print(treeObj.getSum(5))
-    print(treeObj.getSum(6))
+
+    # change value as 5 of original array at index 5
+    treeObj.updateOriginalArray(val=5, index=5)
+    print(treeObj.getSum(5))
